a0c620ee03fedf2292a2d41d025f7a3caec0c619
Erick Alves <erick@erickalves.com.br>
2020-05-07 06:21:15 +0100

    fixes #1

--- src/Components/Message/Types/ImageMessage/index.js
+++ src/Components/Message/Types/ImageMessage/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import {Container, Image} from './styles';
 
-const ImageMessage = ({children, imageUrl}) => {
+const ImageMessage = ({imageUrl}) => {
   return (
     <Container>
       <Image source={{uri: imageUrl}} />
