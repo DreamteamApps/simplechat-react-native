f4222b38c882602c4dad2178653f5c05e848710f
Erick Alves <erick@erickalves.com.br>
2020-05-07 01:42:17 +0100

    Fixing android upload file

--- src/Service/fileApi.js
+++ src/Service/fileApi.js
@@ -1,10 +1,10 @@
-import axios from 'axios';
+import {Platform} from 'react-native';
 import {SERVER_URL} from '~/Utils/constants';
 
 export async function uploadSound(filePath, duration) {
   const data = new FormData();
   const audio = {
-    uri: filePath,
+    uri: Platform.OS === 'android' ? `file://${filePath}` : filePath,
     type: 'audio/wav',
     name: 'teste2.wav',
   };
@@ -13,8 +13,23 @@ export async function uploadSound(filePath, duration) {
   data.append('type', 'audio');
   console.log('filePath', filePath);
   try {
-    var response = await axios.post(`${SERVER_URL}file/upload`, data);
-    console.log(response);
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', `${SERVER_URL}file/upload`);
+    xhr.send(data);
+    xhr.onreadystatechange = (e) => {
+      if (xhr.readyState !== 4) {
+        return;
+      }
+
+      if (xhr.status === 200) {
+        console.log('success', xhr.responseText);
+      } else {
+        console.log('error', xhr.responseText);
+      }
+    };
+
+    //not working in react native 0.62 version :(
+    // var response = await axios.post(`${SERVER_URL}file/upload`, data);
   } catch (error) {
     console.log(Object.keys(error));
     console.log(error.message);
