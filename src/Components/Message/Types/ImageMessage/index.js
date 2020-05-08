14f8f7b2da26937d995e4a5433bc1e9441b2ed2a
Erick Alves <erick@erickalves.com.br>
2020-05-08 14:45:30 +0100

    Image with thumbnail

--- src/Components/Message/Types/ImageMessage/index.js
+++ src/Components/Message/Types/ImageMessage/index.js
@@ -4,7 +4,7 @@ import {Container, Image} from './styles';
 import {useNavigation} from '@react-navigation/native';
 import Lightbox from 'react-native-lightbox';
 
-const ImageMessage = ({imageUrl}) => {
+const ImageMessage = ({imageUrl, thumbUrl}) => {
   const navigation = useNavigation();
   const [imageResize, setImageResize] = useState('cover');
 
@@ -14,8 +14,11 @@ const ImageMessage = ({imageUrl}) => {
         springConfig={{tension: 15, friction: 7}}
         onOpen={() => setImageResize('contain')}
         onClose={() => setImageResize('cover')}
-        willClose={() => setImageResize('cover')}>
-        <Image resizeMode={imageResize} source={{uri: imageUrl}} />
+        willClose={() => setImageResize('cover')}
+        renderContent={() => {
+          return <Image resizeMode={imageResize} source={{uri: imageUrl}} />;
+        }}>
+        <Image resizeMode={'contain'} source={{uri: thumbUrl}} />
       </Lightbox>
     </Container>
   );
